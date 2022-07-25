19ab489aa8cca130985cfecc423a4f8abcef2b4a
Chet Husk <chusk3@gmail.com>
2022-07-25 15:31:23 -0500

    rewrite to program-main style for familiarity

--- 01-hello-world/Program.cs
+++ 01-hello-world/Program.cs
@@ -1,5 +1,15 @@
-﻿/// <summary>
-/// Greet someone politely
-/// </summary>
-/// <param name="person">The name of the person to greet. This will be provided by an end user on the command line.</param>
-void Greet(string person) => Console.WriteLine($"Hello, {person}");
+﻿namespace HelloWorld;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    }
+
+    /// <summary>
+    /// Greet someone politely
+    /// </summary>
+    /// <param name="person">The name of the person to greet. This will be provided by an end user on the command line.</param>
+    void Greet(string person) => Console.WriteLine($"Hello, {person}");
+}
+
